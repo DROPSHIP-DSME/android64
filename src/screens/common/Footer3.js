efc2e0280355faaabd03ad8996df70a391a7bfed
CRobinson <crobinson@simplyclc.com>
2022-10-05 13:38:33 -0400

    footer update

--- src/screens/common/Footer3.js
+++ src/screens/common/Footer3.js
@@ -147,36 +147,6 @@ const Footer3 = (props) => {
 
     return (
         <View>
-            {showpop==true &&
-                <View style={{flex:1,position:'absolute',zIndex:8003,bottom:50,left:60, borderRadius:10, backgroundColor:'#ffffff'}}>
-                    <View onPress={() => { setshowaccountpop(false); setshowpop(false); navigation.navigate('shop'); }}  style={tw.style('inline-block items-center mx-3 mt-4 md:px-2 md:mx-2')}>
-                        <TouchableOpacity onPress={() => { setshowaccountpop(false); setshowpop(false); navigation.navigate('shop'); }}>
-                            <Text onPress={() => { setshowaccountpop(false); setshowpop(false);navigation.navigate('shop'); }} style={tw.style('text-sm text-right font-bold text-gray-700')}>Shop Products</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View onPress={() => { setshowaccountpop(false); setshowpop(false); navigation.navigate('upcoming'); }}  style={tw.style('inline-block items-center px-2 mt-3 mx-3 md:px-2 md:mx-2')}>
-                        <TouchableOpacity onPress={() => { setshowaccountpop(false); setshowpop(false); navigation.navigate('upcoming'); }}>
-                            <Text onPress={() => { setshowaccountpop(false); setshowpop(false);navigation.navigate('upcoming'); }} style={tw.style('text-sm text-right font-bold text-gray-700',{marginBottom:10})}>View Livestreams</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            }
- 
-            {showaccountpop==true &&
-                <View style={{flex:1,position:'absolute',width:150,zIndex:8003,bottom:50,right:20, borderRadius:10, backgroundColor:'#ffffff'}}>
-                    <View onPress={() => {setshowaccountpop(false); setshowpop(false); navigation.navigate('Account'); }}  style={tw.style('inline-block items-center mx-3 mt-4 md:px-2 md:mx-2')}>
-                        <TouchableOpacity onPress={() => { setshowaccountpop(false); setshowpop(false);navigation.navigate('Account'); }}>
-                            <Text onPress={() => {setshowaccountpop(false); setshowpop(false); navigation.navigate('Account'); }} style={tw.style('text-sm text-right font-bold text-gray-700')}>My Account</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View onPress={() => { setshowaccountpop(false); setshowpop(false); navigation.navigate('Overview'); }}  style={tw.style('inline-block items-center px-2 mt-3 mx-3 md:px-2 md:mx-2')}>
-                        <TouchableOpacity onPress={() => { setshowaccountpop(false); setshowpop(false);navigation.navigate('Overview'); }}>
-                            <Text onPress={() => { setshowaccountpop(false); setshowpop(false); navigation.navigate('Overview'); }} style={tw.style('text-sm text-right font-bold text-gray-700',{marginBottom:10})}>My Store</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            }
-
 
         <View style={tw.style('max-w-fit  bg-white flex h-15 px-6 py-2 md:py-4 md:px-6 sm:mx-1 md:mx-2')}>
           <View style={tw.style('flex-row justify-between')}>
@@ -198,50 +168,29 @@ const Footer3 = (props) => {
               </View>
             }
         </TouchableOpacity>
-<<<<<<< HEAD
-        {/*
-        <TouchableOpacity onPress={() => { navigation.navigate('upcoming'); }}>
-            {onSelection==2 ?
-              <View style={tw.style('inline-block items-center px-2 mx-1 md:px-2 md:mx-2')}>
-                  <Text>
-                     <VideoCameraIcon color="red" fill="#b80000" size={24} />
-                  </Text>
-                   <Text style={tw.style('text-sm text-right font-normal text-red-700')}>Livestreams</Text>
-              </View>
-          :
-              <View style={tw.style('inline-block items-center px-2 mx-1 md:px-2 md:mx-2')}>
-                  <Text>
-                     <VideoCameraIcon color="red" fill="gray" size={24} />
-                  </Text>
-                   <Text style={tw.style('text-sm text-right font-normal text-gray-700')}>Livestreams</Text>
-              </View>
-            }
-        </TouchableOpacity>
-        */}
 
         
-        <TouchableOpacity onPress={() => { setshowaccountpop(false); setshowpop(s=>!s); setTimeout(function(){ setshowpop(false); },10000) }}>
-         {onSelection==2 ?
-           <View style={tw.style('inline-block items-center px-2 mx-1 md:px-2 md:mx-2')}>
-               <Text>
-                  <ShoppingBagIcon color="red" fill="#b80000" size={24} />
-               </Text>
-                <Text style={tw.style('text-sm text-right font-normal text-red-700')}>Shop</Text>
-           </View>
-       :
-           <View style={tw.style('inline-block items-center px-2 mx-1 md:px-2 md:mx-2')}>
-               <Text>
-                  <ShoppingBagIcon color="red" fill="gray" size={24} />
-               </Text>
-                <Text style={tw.style('text-sm text-right font-normal text-gray-700')}>Shop</Text>
-           </View>
-        }
-        </TouchableOpacity>
-=======
+            {/* <TouchableOpacity onPress={() => { setshowaccountpop(false); setshowpop(s=>!s); setTimeout(function(){ setshowpop(false); },10000) }}>
+            {onSelection==2 ?
+            <View style={tw.style('inline-block items-center px-2 mx-1 md:px-2 md:mx-2')}>
+                <Text>
+                    <ShoppingBagIcon color="red" fill="#b80000" size={24} />
+                </Text>
+                    <Text style={tw.style('text-sm text-right font-normal text-red-700')}>Shop</Text>
+            </View>
+        :
+            <View style={tw.style('inline-block items-center px-2 mx-1 md:px-2 md:mx-2')}>
+                <Text>
+                    <ShoppingBagIcon color="red" fill="gray" size={24} />
+                </Text>
+                    <Text style={tw.style('text-sm text-right font-normal text-gray-700')}>Shop</Text>
+            </View>
+            }
+            </TouchableOpacity> */}
+
        
         {/* showShopLinks selection component */}
         <Showshoplinks/>
->>>>>>> 69dc1145a51011c383fa9e0332140d19f91580a5
 
 
         <TouchableOpacity onPress={() => { setshowaccountpop(false); setshowpop(false); navigation.navigate("Dashlive")}} >
@@ -263,29 +212,27 @@ const Footer3 = (props) => {
 
         </TouchableOpacity>
 
-<<<<<<< HEAD
 
-        <TouchableOpacity onPress={() => { setshowpop(false); setshowaccountpop(s=>!s); setTimeout(function(){ setshowaccountpop(false); },10000) }}>
-         {onSelection==5 ?
-           <View style={tw.style('inline-block items-center px-2 mx-1 md:px-2 md:mx-2')}>
-               <Text>
-                  <UserIcon color="red" fill="#b80000" size={24} />
-               </Text>
-                <Text style={tw.style('text-sm text-right font-normal text-red-700')}>Account</Text>
-           </View>
-       :
-           <View style={tw.style('inline-block items-center px-2 mx-1 md:px-2 md:mx-2')}>
-               <Text>
-                  <UserIcon color="red" fill="gray" size={24} />
-               </Text>
-                <Text style={tw.style('text-sm text-right font-normal text-gray-700')}>Account</Text>
-           </View>
-        }
-        </TouchableOpacity>
-=======
+            {/* <TouchableOpacity onPress={() => { setshowpop(false); setshowaccountpop(s=>!s); setTimeout(function(){ setshowaccountpop(false); },10000) }}>
+            {onSelection==5 ?
+            <View style={tw.style('inline-block items-center px-2 mx-1 md:px-2 md:mx-2')}>
+                <Text>
+                    <UserIcon color="red" fill="#b80000" size={24} />
+                </Text>
+                    <Text style={tw.style('text-sm text-right font-normal text-red-700')}>Account</Text>
+            </View>
+        :
+            <View style={tw.style('inline-block items-center px-2 mx-1 md:px-2 md:mx-2')}>
+                <Text>
+                    <UserIcon color="red" fill="gray" size={24} />
+                </Text>
+                    <Text style={tw.style('text-sm text-right font-normal text-gray-700')}>Account</Text>
+            </View>
+            }
+            </TouchableOpacity> */}
+        
         {/* Account links selection component */}
         <Showaccountlinks/>
->>>>>>> 69dc1145a51011c383fa9e0332140d19f91580a5
 
         </View>
 
